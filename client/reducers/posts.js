1c6a84627583685b1c05eec600a58c18ccd87724
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2016-11-02 10:10:02 +0000

    Combinine reducers added

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,5 +1,9 @@
 // A reducer takes in two things:
 // 1. The action (info about what happened)
 // 2. A copy of the current state
+// The reducer eturns copy of the updated store
 
- 
+export default function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
